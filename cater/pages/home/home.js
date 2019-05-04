e2aca2b3feaac2ae8a6a70f0830917448de31822
hyattsoft <hyattsoft@sina.com>
2019-05-05 06:26:51 +0800

    凯悦云餐饮管理系统 v1.0.0

---
+++ cater/pages/home/home.js
@@ -0,0 +1,14 @@
+var app = getApp();
+Page({
+    data:{
+        imgUrls:[
+            'https://cdn.hyattsoft.cn/images/banner.jpg',
+            'https://cdn.hyattsoft.cn/images/banner.jpg',
+            'https://cdn.hyattsoft.cn/images/banner.jpg'
+        ],
+        indicatorDots:true,
+        autoPlay:true,
+        interval:3000,
+        duration:1000
+    }
+});
