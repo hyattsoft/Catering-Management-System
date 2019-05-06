488bc29eb7439056b9b25da5b445d5e2af24b121
hyattsoft <hyattsoft@sina.com>
2019-05-07 07:18:34 +0800

    凯悦云餐饮管理系统 v1.0.0

---
+++ client/__init__.py
@@ -0,0 +1,5 @@
+# _*_ coding:utf-8 _*_
+# author: hyatt
+# CreateDateTime: 2019-05-07 7:17
+# FileName: __init__.py.py
+# Email: kinagod@sina.com
---
+++ client/client.py
@@ -0,0 +1,5 @@
+# _*_ coding:utf-8 _*_
+# author: hyatt
+# CreateDateTime: 2019-05-07 7:16
+# FileName: client.py
+# Email: kinagod@sina.com
